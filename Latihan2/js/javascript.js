b61e373339c9a813d9094d82694e0180da251a50
IndroMan <indrodwi2@gmail.com>
2022-09-29 09:34:32 +0800

    Belajar JS

---
+++ Latihan2/js/javascript.js
@@ -0,0 +1,49 @@
+// console.log("Hello World");
+// alert("AWAS ADA ANJING GALAK!!!");
+// var nama = confirm("are you sure you want to continue?");
+// if(nama == true){
+//     console.log("OKE ANDA SUDAH YAKIN");
+// }
+// else{
+//     console.log("BELUM YAKIN");
+// }
+
+// var nama = prompt("Masukkan nama anda : ");
+// console.log(nama);
+
+// const h1 = document.getElementById("judul");
+// h1.innerHTML = "Indro Mamen"
+// h1.style.color = "red";
+
+// const p = document.getElementsByTagName("p");
+// p[1].style.color = "blue";
+// for(let i = 0; i<p.length; i++){
+//     p[i].style.color = "blue";
+//     p[i].style.fontWeight = "bold";
+//     p[i].style.fontSize = "1.5em";
+// }
+
+// dom3 = document.getElementsByClassName("item");
+// dom3[0].style.color = "red";
+
+// function press(){
+//     const ul = document.querySelector("section#query-slctr ul");
+    
+//     const li = document.createElement("li");
+//     const item = document.createTextNode("Item 4");
+
+//     li.appendChild(item);
+//     ul.appendChild(li);
+// }
+
+// const press = document.getElementById("btn-info");
+// press.addEventListener("click", function(){
+//     const x = document.getElementById("info");
+//     if(x.style.display == "none"){
+//         x.style.display = "block";
+//     }
+//     else{
+//         x.style.display = "none";
+//     }
+// });
+
